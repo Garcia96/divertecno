d4241acdda4266b857af5239016572d4ce22f622
Nicolas Garcia <cristanico@hotmail.com>
2023-03-28 17:40:57 -0500

    Pagina de detalle de juego

--- src/app/pages/vr-games/vr-game/vr-game.component.ts
+++ src/app/pages/vr-games/vr-game/vr-game.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VrGames } from 'src/app/utils/games';
 
 @Component({
   selector: 'app-vr-game',
   templateUrl: './vr-game.component.html',
-  styleUrls: ['./vr-game.component.css']
+  styleUrls: ['./vr-game.component.css'],
 })
 export class VrGameComponent implements OnInit {
-
-  constructor() { }
+  gameData: any = [];
+  vrGames = new VrGames();
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.gameData = this.vrGames
+      .getGames()
+      .data.find((game) => game.id === Number(id));
   }
-
 }
